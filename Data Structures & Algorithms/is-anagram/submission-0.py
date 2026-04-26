926f068186335cd8697a4ca808f1e1888d03c6f4
Kosha Gohil <160976445+KoshaG0hil@users.noreply.github.com>
2026-04-26 19:17:53 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+      
+        new_sdict= {}
+        new_tdict= {}
+
+        if len(s)!= len(t):
+            return False
+        
+       
+        for char in s:
+            if char in new_sdict:
+                new_sdict[char] = new_sdict[char] + 1
+            else:
+                new_sdict[char]=1
+
+
+        for char in t:
+            if char in new_tdict:
+                new_tdict[char] = new_tdict[char] + 1
+            else:
+                new_tdict[char]=1
+
+        if new_sdict==new_tdict:
+            return True
+
+        else:
+            return False
+        
