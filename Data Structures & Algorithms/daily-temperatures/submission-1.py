c56f7c237d7588f8dcb9627ffa026ed1354e8d8a
Kosha Gohil <160976445+KoshaG0hil@users.noreply.github.com>
2026-04-29 14:00:19 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+          
+            n=len(temperatures)
+            answer=[0]* n   # u set all the indices to 0 in the list as of now 0 *n 
+
+            stack=[]
+
+            for i,t in enumerate(temperatures):   # for index is i and current temp is t
+
+                while stack and stack[-1][0]  < t:    #stack(a,b)  b= temp is < then temp of i+1
+
+                    x,y= stack.pop()     # as we have use enumerate it gives tuple and have to pop with tuple u cant pop either one
+                    answer[y]=i-y         # in the list where we want to return num of days by current index - previos index we do asnwer[stack_i] to update only previous day index and
+                                                    # if u write answer[i] it will assume current i 
+                stack.append((t,i))
+            return answer
+
