a8267671e68602e35af347cf69067274fe723be9
Kosha Gohil <160976445+KoshaG0hil@users.noreply.github.com>
2026-04-30 19:29:47 -0400

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+  
+        longest=0
+        l=0
+        counts=[0] *26   # 26 alphabets or UPPERCASE LETTERS
+        
+        for r in range(len(s)):
+            counts[ord(s[r])-65] +=1           
+            while (r-l+1) - max(counts) > k:
+                counts[ord(s[l])-65] -=1
+                l=l+1
+
+            longest=max(longest,(r-l+1))
+        
+        return longest
+
+
+
